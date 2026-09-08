1e3d0f67211be0d9b8898028ecb2cc96f400688c
Mounir Lmaimouni <mounirmaimouni21@gmail.com>
2026-09-08 16:24:37 +0100

    algo

---
+++ Algo2_Chercher_la_valeur_maximal/app.js
@@ -0,0 +1,17 @@
+
+let numbers = [12, 45, 7, 89, 23, 56, 91, 34];
+
+ let x = numbers[0];
+
+for(let i = 1 ; i < numbers.length ;i++){
+      
+       let Y = numbers[i];
+
+       if(Y > x){
+          x = Y ;
+       }
+}
+ console.log(x);
+
+
+
