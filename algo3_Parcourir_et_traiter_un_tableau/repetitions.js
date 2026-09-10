345b5af61b14d05c5422ff08d95b5340f853601a
Mounir Lmaimouni <mounirmaimouni21@gmail.com>
2026-09-10 12:37:28 +0100

    algo3

---
+++ algo3_Parcourir_et_traiter_un_tableau/repetitions.js
@@ -0,0 +1,48 @@
+let numbers = [4, 7, 2, 7, 9, 4, 5]; 
+let rptNumbers = []; 
+
+for(let i = 0; i < numbers.length; i++){ 
+
+    let counter = 0;
+
+    for(let x = 0; x < numbers.length; x++){ 
+
+        if(numbers[i] == numbers[x]){ 
+            counter++;
+        }
+    }
+
+    if(counter > 1){
+
+        let alreadyExist = false;
+
+        for(let x = 0; x < i; x++){
+
+            if(numbers[i] == numbers[x]){
+                alreadyExist = true;
+            }
+        }
+
+        if(alreadyExist == false){
+            rptNumbers.push(numbers[i]);
+        }
+    }
+}
+
+console.log(rptNumbers);
+
+
+
+
+
+
+
+
+  
+    /*  let itemNumber = numbers[i];
+     if(itemNumber == numbers[i]){
+        coumpter +=1;
+        if(coumpter > 1){
+            rptNumbers.push(this.numbers[i]);
+        }
+     }*/
